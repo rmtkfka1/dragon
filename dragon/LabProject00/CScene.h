441c43def23fe05b9e20dff052f03ec79f67d806
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2024-05-22 17:07:26 +0900

    8까지완료

---
+++ dragon/LabProject00/CScene.h
@@ -0,0 +1,43 @@
+#pragma once
+class CShader;
+class CCamera;
+class CGameObject;
+
+class CScene
+{
+public:
+	CScene();
+	~CScene();
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM
+		lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM
+		lParam);
+	//void CreateGraphicsRootSignature(ID3D12Device* pd3dDevice);
+	void CreateGraphicsPipelineState(ID3D12Device* pd3dDevice);
+	void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	void ReleaseObjects();
+	bool ProcessInput(UCHAR* pKeysBuffer);
+	void AnimateObjects(float fTimeElapsed);
+	void PrepareRender(ID3D12GraphicsCommandList* pd3dCommandList);
+	void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
+
+	void ReleaseUploadBuffers();
+	ID3D12RootSignature* CreateGraphicsRootSignature(ID3D12Device* pd3dDevice);
+	ID3D12RootSignature* GetGraphicsRootSignature();
+
+	CShader** m_ppShaders = NULL;
+	int m_nShaders = 0;
+
+	ID3D12RootSignature* m_pd3dGraphicsRootSignature = NULL;
+	//루트 시그너쳐를 나타내는 인터페이스 포인터이다. 
+	ID3D12PipelineState *m_pd3dPipelineState = NULL;
+	//파이프라인 상태를 나타내는 인터페이스 포인터이다
+
+	CGameObject** m_ppObjects = NULL;
+	int m_nObjects = 0;
+
+
+
+
+};
+
